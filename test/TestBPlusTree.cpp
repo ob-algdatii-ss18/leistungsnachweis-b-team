465304cc6aa10683892136b68484458ac9236c29
Unknown <matthias.herpers@gmail.com>
2018-06-11 12:14:02 +0200

    test another 1

--- test/TestBPlusTree.cpp
+++ test/TestBPlusTree.cpp
@@ -821,7 +821,7 @@ TEST(SortList, InsertTwoElementsRemoveTheLowerKeyElement) {
 }
  */
 
-
+//worked 
 TEST(SortList, InsertTwoElementsRemoveTheUpperKeyElement) {
     std::function<int(string)> keyConverter = [](string s) { return (int) s.size(); };
     const string data1 = "tes";
@@ -842,7 +842,7 @@ TEST(SortList, InsertTwoElementsRemoveTheUpperKeyElement) {
     ASSERT_EQ(*result1, data1);
     ASSERT_EQ(result2, nullptr);
 }
-/*
+
 TEST(SortList, InsertThreeElementsRemoveLeft) {
     std::function<int(string)> keyConverter = [](string s) { return (int) s.size(); };
     const string data1 = "test";
@@ -869,7 +869,7 @@ TEST(SortList, InsertThreeElementsRemoveLeft) {
     ASSERT_EQ(result2, nullptr);
     ASSERT_EQ(*result3, data3);
 }
-
+/*
 TEST(SortList, InsertThreeElementsRemoveMiddle) {
     std::function<int(string)> keyConverter = [](string s) { return (int) s.size(); };
     const string data1 = "test";
